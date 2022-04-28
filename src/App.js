173e25f9dc6327ad2fe20635683c0fd04c9ba0f4
bluskymanner <page111first@gmail.com>
2022-04-28 05:40:24 +0200

    added app.js and index.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Table from './Table';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Table />
+    </>
+  );
+}
+
+export default App;
